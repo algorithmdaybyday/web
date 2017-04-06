4cde0613a89749834b00af689c7ef99146ef544f
YanLi26 <yali2241@colorado.edu>
2017-04-06 00:11:34 -0600

    add bootstrap and navbar

--- src/modules/home.vue
+++ src/modules/home.vue
@@ -1,10 +1,26 @@
 <template>
-    <div>
-        <span>{{msg}}</span>
+<div id="home">
+<nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <router-link to='/' class="navbar-brand">Algorithm Day by Day</router-link>
+        </div>
+        <ul class="nav navbar-nav">
+            <li class="active"><router-link to='/'>Home</router-link></li>
+            <li><a href="#">Page 1</a></li>
+            <li><a href="#">Page 2</a></li>
+            </ul>
+        <ul class="nav navbar-nav navbar-right">
+            <li><a href="https://github.com/algorithmdaybyday/web"> Github </a></li>
+            <li><a href="#"> Login</a></li>
+        </ul>
     </div>
+</nav>
+<h2>{{msg}}</h2>
+</div>
 </template>
-<script>
 
+<script>
 export default {
     data(){
         return {
@@ -14,3 +30,7 @@ export default {
 }
 
 </script>
+
+<style scoped>
+
+</style>
